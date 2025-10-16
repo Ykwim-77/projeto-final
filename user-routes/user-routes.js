4210171863485583b2ca4b5eda3c98050063e08a
Christian <christian.darosa0106@gmail.com>
2025-10-16 19:34:51 -0300

    pimeiro commit com node

---
+++ user-routes/user-routes.js
@@ -0,0 +1,43 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+//tipos de usuarios
+//admin
+//user
+
+
+//rotas de usuarios
+//get
+    //pegar um usuario
+    //pegar todos os usuarios
+//post
+    //criar um usuario
+//delete
+//put
+    //atualizar um usuario
+
+
+
+
+
+//rotas de produtos
+//get
+    //pegar um produto
+    //pegar todos os produtos
+//post
+    //criar um produto
+    //reservar um produto
+    //entregar um produto
+//delete
+//put
+    //atualizar um produto
