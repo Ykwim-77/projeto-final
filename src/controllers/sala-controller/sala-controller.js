729aaa2be0729d6f99468cc8129c6a50d2f4a280
kaio0980 <trindadekaio09@gamil.com>
2025-10-20 20:53:20 -0300

    novo controller

--- src/controllers/sala-controller/sala-controller.js
+++ src/controllers/sala-controller/sala-controller.js
@@ -1,4 +1,85 @@
+import { PrismaClient } from "@prisma/client/extension";
 
-async function pegartodasSalas(req,res) {
-    
+
+
+const prisma = new PrismaClient();
+
+
+async function pegarTodasSalas(req,res) {
+    const salas = await prisma.sala.findMany();
+    res.json(salas);
+
+    if (!salas) {
+        return res.status(404).json({ mensagem: "Nenhuma sala encontrada." });
+    }
+
+    try {
+        return res.status(200).json(salas);
+    } catch (error) {
+        return res.status(500).json({ mensagem: "Erro ao buscar salas." });
+    }
+
 }
+
+async function pegar1Sala(req,res) {
+    const { id } = req.params;
+    const sala = await prisma.sala.findUnique({
+        where: { id: Number(id) }
+    });
+    res.json(sala);
+}
+
+async function criarSala(req,res) {
+    const { nome, capacidade } = req.body;
+    const sala = await prisma.sala.create({
+        data: { nome, capacidade }
+    });
+    res.json(sala);
+}
+
+async function atualizarSala(req,res) {
+    const { id } = req.params;
+    const { nome, capacidade } = req.body;
+    const sala = await prisma.sala.update({
+        where: { id: Number(id) },
+        data: { nome, capacidade }
+    });
+    res.json(sala);
+}
+
+async function deletarSala(req,res) {
+    const { id } = req.params;
+    await prisma.sala.delete({
+        where: { id: Number(id) }
+    });
+    res.sendStatus(204);
+}
+
+async function reservarSala(req,res) {
+    const { id } = req.params;
+    const reserva = await prisma.reserva.create({
+        data: {
+            sala: { connect: { id: Number(id) } },
+            usuario: { connect: { id: req.user.id } }
+        }
+    });
+    res.json(reserva);
+}
+
+async function liberarSala(req,res) {
+    const { id } = req.params;
+    await prisma.reserva.deleteMany({
+        where: { salaId: Number(id) }
+    });
+    res.sendStatus(204);
+}
+
+export default {
+    pegarTodasSalas,
+    pegar1Sala,
+    criarSala,
+    atualizarSala,
+    deletarSala,
+    reservarSala,
+    liberarSala
+};
