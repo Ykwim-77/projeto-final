2bcc49672a819c3564bc8bbde83db4e226d1376c
Christian <christian.darosa0106@gmail.com>
2025-10-21 21:40:00 -0300

    começo do user-controller-

--- src/controllers/user-controller/user-controller.js
+++ src/controllers/user-controller/user-controller.js
@@ -17,26 +17,55 @@ const prisma = new PrismaClient()
 
 
 async function pegar1Usuario(req, res){
-    const idNumber = (req.params.id)
+    const idNumber = parseInt(req.params.id)
+    if (isNaN(idNumber)) {
+        return res.status(400).json({ mensagem: "o id precisa ser um número inteiro" });
+    } 
+
     try{
-        const usuario = prisma.usuario.findUnique({
+        const usuario = await prisma.usuario.findUnique({
             where:{
                 id_usuario: idNumber
             }
         });
-        return res.status(200).json({Usuario_encontrado:`o usuário ${usuario.nome} foi encontrado`}).json(usuario)
+        return res.status(200).json(usuario)
 
     }catch(error){
-        return res.status(500).json({moio:"deu ruim na pegar 1 usuario"})
+        return res.status(500).json({moio:"deu ruim na pegar 1 usuario", error})
     }
 
 }
 
-async function pegarTodosUsuarios(req, res){
+async function pegarTodosUsuarios(req, res){ // acabar o pegartodos os usuarios
+    try{
+
+    }catch(error){
+        return res.status(500).json({pegartodosusuario:"deu pau pra pegar todos os usuarios ", error})
+    }
 
 }
 
-async function criarUsuario(req, res){
+async function criarUsuario(req, res){ //acabar ao criar
+    try{
+        const {nome, email, senha_hash, id_tipo_usuario, ativo, cpf} = req.body
+
+        await prisma.usuario.create({
+            data:{
+                nome: nome,
+                email:email,
+                senha_hash: senha_hash,
+                id_tipo_usuario: id_tipo_usuario,
+                ativo: ativo,
+                cpf: cpf
+            }
+        });
+
+        return res.status(201).json({mensagem:"usuario foi criado com sucesso"});
+    }catch(error){
+        return res.status(500).json({criar_usuario:"ve oq q aconteceu para criar o usuario ai paizão"});
+    }
+
+
 
 }
 async function atualizarUsuario(req, res){
