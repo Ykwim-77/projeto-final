e362502991441483258635cd3dfe03bb213b6cc5
Christian <christian.darosa0106@gmail.com>
2025-10-24 23:50:59 -0300

    Merge para main 0.1.004

--- frontend/src/server.ts
+++ frontend/src/server.ts
@@ -38,10 +38,10 @@ app.use(
 /**
  * Handle all other requests by rendering the Angular application.
  */
-app.use((req, res, next) => {
+app.use((req: any, res: any, next: () => any) => {
   angularApp
     .handle(req)
-    .then((response) =>
+    .then((response: any) =>
       response ? writeResponseToNodeResponse(response, res) : next(),
     )
     .catch(next);
@@ -53,7 +53,7 @@ app.use((req, res, next) => {
  */
 if (isMainModule(import.meta.url) || process.env['pm_id']) {
   const port = process.env['PORT'] || 4000;
-  app.listen(port, (error) => {
+  app.listen(port, (error: any) => {
     if (error) {
       throw error;
     }
