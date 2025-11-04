d16df9e858ddefc75040dee3cfef4ef8e33cc1a7
rafaelpilonetto <rafaelpilonetto59@gmail.com>
2025-11-03 22:01:33 -0300

    0.8.005

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,15 +1,22 @@
-// src/app/app.routes.ts
 import { Routes } from '@angular/router';
-import { LoginComponent } from './pages/login/login';
-import { HomeComponent } from './pages/home/home';
-import { ProdutosComponent } from './pages/produtos/produtos';
-import { CadastroComponent } from './pages/cadastro/cadastro';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
-  { path: 'cadastro', component: CadastroComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'produtos', component: ProdutosComponent },
+  { 
+    path: 'login', 
+    loadComponent: () => import('./pages/login/login').then(m => m.LoginComponent)
+  },
+  { 
+    path: 'cadastro', 
+    loadComponent: () => import('./pages/cadastro/cadastro').then(m => m.CadastroComponent)
+  },
+  { 
+    path: 'home', 
+    loadComponent: () => import('./pages/home/home').then(m => m.HomeComponent)
+  },
+  { 
+    path: 'produtos', 
+    loadComponent: () => import('./pages/produtos/produtos').then(m => m.ProdutosComponent)
+  },
   //{ path: '**', redirectTo: '/login' }
 ];
